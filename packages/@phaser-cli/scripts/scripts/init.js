6e1a6069f8a134a61857af2ef3c95b1b5778e234
Brendan Goodenough <brendan@goodenough.nz>
2018-03-10 11:53:01 +1300

    Add init script for copying template

---
+++ packages/@phaser-cli/scripts/scripts/init.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const chalk = require('chalk')
+const fs = require('fs-extra')
+const path = require('path')
+
+module.exports = (appPath, appName) => {
+  const ownPackageName = require(path.join(__dirname, '..', 'package.json')).name
+  const ownPath = path.join(appPath, 'node_modules', ownPackageName)
+
+  const templatePath = path.join(ownPath, 'template')
+
+  if (!fs.existsSync(templatePath)) {
+    console.error(`Could not locate supplied template: ${chalk.green(templatePath)}`)
+    return
+  }
+
+  fs.copySync(templatePath, appPath)
+
+  console.log(`Success! Created ${appName} at ${appPath}`)
+}
