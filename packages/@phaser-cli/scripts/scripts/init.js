20a8e6510314de1b85a42a7b78e5a99ca2060f83
Brendan Goodenough <brendan@goodenough.nz>
2018-03-10 15:02:30 +1300

    Add scripts when creating package file

--- packages/@phaser-cli/scripts/scripts/init.js
+++ packages/@phaser-cli/scripts/scripts/init.js
@@ -7,6 +7,18 @@ const path = require('path')
 module.exports = (appPath, appName) => {
   const ownPackageName = require(path.join(__dirname, '..', 'package.json')).name
   const ownPath = path.join(appPath, 'node_modules', ownPackageName)
+  const appPackage = require(path.join(appPath, 'package.json'))
+
+  appPackage.dependencies = appPackage.dependencies || {}
+
+  appPackage.scripts = {
+    start: 'phaser-scripts start'
+  }
+
+  fs.writeFileSync(
+    path.join(appPath, 'package.json'),
+    JSON.stringify(appPackage, null, 2)
+  )
 
   const templatePath = path.join(ownPath, 'template')
 
