5dc26a0c5ccc625ab9b30dc188b7baa5d2805359
Brendan Goodenough <brendan@goodenough.nz>
2018-03-13 08:09:58 +1300

    Print tidier error messages

--- packages/@phaser-cli/scripts/scripts/build.js
+++ packages/@phaser-cli/scripts/scripts/build.js
@@ -2,10 +2,52 @@
 
 process.env.NODE_ENV = 'production'
 
+const chalk = require('chalk')
 const webpack = require('webpack')
 const webpackConfig = require('../config/webpack.config')
 
 build()
+  .then(({ stats, warnings }) => {
+    if (warnings.length) {
+      console.log(chalk.yellow('Compiled with warnings'))
+      console.log(warnings.join('\n\n'))
+      return
+    }
+
+    console.log(chalk.green('Compiled successfully!\n'))
+  })
+  .catch(err => {
+    console.log(chalk.red('Failed to compile.'))
+    console.log(err)
+    process.exit(1)
+  })
+
+const formatMessage = (message, type) => {
+  const lines = message.split('\n')
+
+  if (lines[0].lastIndexOf('!') !== -1) {
+    lines[0] = lines[0].substr(lines[0].lastIndexOf('!') + 1)
+  }
+
+  message = lines.join('\n')
+  return message.trim()
+}
+
+const formatWebpackMessages = json => {
+  const errors = json.errors.map(message => formatMessage(message, 'error'))
+  const warnings = json.warnings.map(message => formatMessage(message, 'warning'))
+
+  if (errors.length > 1) {
+    errors.length = 1
+  }
+
+  const result = {
+    errors,
+    warnings
+  }
+
+  return result
+}
 
 function build () {
   const compiler = webpack(Object.assign({}, webpackConfig, { mode: 'production' }))
@@ -16,8 +58,15 @@ function build () {
         return reject(err)
       }
 
-      // TODO: Format this to be nicer
-      console.log(stats)
+      const messages = formatWebpackMessages(stats.toJson({}, true))
+      if (messages.errors.length) {
+        return reject(new Error(messages.errors.join('\n\n')))
+      }
+
+      return resolve({
+        stats,
+        warnings: messages.warnings
+      })
     })
   })
 }
