01313584f21aa7e633371c454533de70c67e8f49
Brendan Goodenough <brendan@goodenough.nz>
2018-03-14 08:51:24 +1300

    Move files to correct path on eject and omit flagged files and content

--- packages/@phaser-cli/scripts/config/paths.js
+++ packages/@phaser-cli/scripts/config/paths.js
@@ -7,8 +7,17 @@ const appDirectory = fs.realpathSync(process.cwd())
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath)
 
 module.exports = {
+  appPath: resolveApp('.'),
   appBuild: resolveApp('build'),
   appEntry: resolveApp('src/main.js'),
   appSrc: resolveApp('src'),
   appHtml: resolveApp('index.html')
 }
+
+// @remove-on-eject-begin
+const resolveOwn = relativePath => path.resolve(__dirname, '..', relativePath)
+
+module.exports = Object.assign({}, module.exports, {
+  ownPath: resolveOwn('.')
+})
+// @remove-on-eject-end
--- packages/@phaser-cli/scripts/scripts/eject.js
+++ packages/@phaser-cli/scripts/scripts/eject.js
@@ -1,7 +1,15 @@
+// @remove-file-on-eject
 'use strict'
 
 const chalk = require('chalk')
+const fs = require('fs-extra')
 const inquirer = require('inquirer')
+const path = require('path')
+
+const paths = require('../config/paths')
+
+const ownPath = paths.ownPath
+const appPath = paths.appPath
 
 inquirer
   .prompt({
@@ -19,4 +27,30 @@ inquirer
     console.log('Ejecting...')
 
     // TODO: Actually eject the project
+    const folders = ['config', 'scripts']
+
+    const files = folders.reduce((files, folder) => {
+      return files.concat(
+        fs
+          .readdirSync(path.join(ownPath, folder))
+          .map(file => path.join(ownPath, folder, file))
+          .filter(file => fs.lstatSync(file).isFile())
+      )
+    })
+
+    folders.forEach(folder => fs.mkdirSync(path.join(appPath, folder)))
+    files.forEach(file => {
+      let content = fs.readFileSync(file, 'utf-8')
+
+      // Skip flagged files
+      if (content.match(/\/\/ @remove-file-on-eject/)) {
+        return
+      }
+
+      // Remove flagged sections of code
+      content = content.replace(/\/\/ @remove-on-eject-begin([\s\S]*?)\/\/ @remove-on-eject-end/gm, '')
+
+      console.log(`Adding ${chalk.cyan(file.replace(ownPath, ''))} to the project`)
+      fs.writeFileSync(file.replace(ownPath, appPath), content)
+    })
   })
